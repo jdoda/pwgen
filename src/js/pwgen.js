a9b6ae7ceb395ff5086ac73882626f54023d42dc
Jonathan Doda <jonathan@jdoda.ca>
2013-07-04 23:08:51 -0400

    Construct password from given charset, instead of base64.

--- src/js/pwgen.js
+++ src/js/pwgen.js
@@ -1,4 +1,6 @@
 function getPassword() {
+    var characterSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+
     var passphrase = document.getElementById("passphrase").value;
     var confirm = document.getElementById("confirm").value;
     var nickname = document.getElementById("nickname").value;
@@ -7,8 +9,20 @@ function getPassword() {
     if (passphrase != confirm) {
         password.value = ("Passphrases didn't match");
     } else {
-        bits = sjcl.misc.pbkdf2(nickname, passphrase)
-        output = sjcl.codec.base64.fromBits(bits)
+        bits = sjcl.misc.pbkdf2(passphrase, nickname);
+        output = constructPassword(sjcl.codec.hex.fromBits(bits), characterSet);
         password.value = output;
     }
+}
+
+function constructPassword(hex, characterSet) {
+    var output = "", i
+    for (i = 0; i < hex.length; i += 2) {
+        var highBits = parseInt(hex.charAt(i), 16) << 4;
+        var lowBits = parseInt(hex.charAt(i+1), 16)
+        var index = highBits + lowBits;
+        index = Math.floor(index * characterSet.length / 256);
+        output += characterSet.charAt(index);
+    }
+    return output;
 }
