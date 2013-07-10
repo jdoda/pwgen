109dd51aac18b91236cbfd0f696692f476ef7c43
Jonathan Doda <jonathan@jdoda.ca>
2013-07-09 23:22:18 -0400

    Major refactoring of the js, implement domain settings (except for required characters).

--- src/js/pwgen.js
+++ src/js/pwgen.js
@@ -1,28 +1,73 @@
-function getPassword() {
-    var characterSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+var pwgen = (function () {
+    "use strict";
 
-    var passphrase = document.getElementById("passphrase").value;
-    var confirm = document.getElementById("confirm").value;
-    var nickname = document.getElementById("nickname").value;
-    var password = document.getElementById("password");
-    
-    if (passphrase != confirm) {
-        password.value = ("Passphrases didn't match");
-    } else {
-        bits = sjcl.misc.pbkdf2(passphrase, nickname);
-        output = constructPassword(sjcl.codec.hex.fromBits(bits), characterSet);
-        password.value = output;
-    }
-}
+    var $ = document.getElementById.bind(document);
 
-function constructPassword(hex, characterSet) {
-    var output = "", i
-    for (i = 0; i < hex.length; i += 2) {
-        var highBits = parseInt(hex.charAt(i), 16) << 4;
-        var lowBits = parseInt(hex.charAt(i+1), 16)
-        var index = highBits + lowBits;
-        index = Math.floor(index * characterSet.length / 256);
-        output += characterSet.charAt(index);
-    }
-    return output;
-}
+    var defaults = {
+        charset : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890`~!@#$%^&*()-=_+[]{}\\|;':\",.<>/?",
+        required : "",
+        index : "0",
+        length : "16"
+    };
+
+    var generatePassword = function () {
+        if ($("passphrase").value !== $("confirm").value) {
+            $("password").value = ("Passphrases didn't match");
+        } else {
+            var bits = sjcl.misc.pbkdf2($("passphrase").value, $("nickname").value + $("index").value);
+            var hex = sjcl.codec.hex.fromBits(bits), output = "", charset = $("charset").value, i;
+
+            var highBits, lowBits, index;
+            for (i = 0; i < hex.length; i += 2) {
+                highBits = parseInt(hex.charAt(i), 16) << 4;
+                lowBits = parseInt(hex.charAt(i + 1), 16);
+                index = highBits + lowBits;
+                index = Math.floor(index * charset.length / 256);
+                output += charset.charAt(index);
+            }
+            $("password").value = output.substr(0, parseInt($("length").value, 10));
+        }
+    };
+
+    var loadDomainSetting = function (domain, setting) {
+        var value = localStorage.getItem("pwgen." + domain + "." + setting);
+        if (value === null) {
+            value = defaults[setting];
+        }
+        return value;
+    };
+
+    var loadAllDomainSettings = function () {
+        ["length", "charset", "required", "index"].forEach(function (setting) {
+            $(setting).value = loadDomainSetting($("nickname").value, setting);
+        });
+    };
+
+    var saveDomainSetting = function (domain, setting, value) {
+        localStorage.setItem("pwgen." + domain + "." + setting, value);
+    };
+
+    var saveDomainSettingForInput = function () {
+        saveDomainSetting($("nickname").value, this.id, this.value);
+    };
+
+    return {
+        "generatePassword" : generatePassword,
+        "loadAllDomainSettings" : loadAllDomainSettings,
+        "saveDomainSettingForInput" : saveDomainSettingForInput
+    };
+}());
+
+window.addEventListener("load", function () {
+    "use strict";
+
+    var $ = document.getElementById.bind(document);
+
+    $("generate").addEventListener("click", pwgen.generatePassword);
+    $("nickname").addEventListener("input", pwgen.loadAllDomainSettings);
+    ["length", "charset", "required", "index"].forEach(function (input) {
+        $(input).addEventListener("input", pwgen.saveDomainSettingForInput);
+    });
+
+    pwgen.loadAllDomainSettings();
+});
