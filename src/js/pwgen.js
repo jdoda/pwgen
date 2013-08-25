bf6d821a344c0a64c1e75eb6ffef5e0a8a78d3ab
Jonathan Doda <jonathan@jdoda.ca>
2013-08-25 16:53:40 -0400

    Implement required characters, defaults loaded from a file, some refactoring, add appcache.

--- src/js/pwgen.js
+++ src/js/pwgen.js
@@ -1,73 +1,88 @@
-var pwgen = (function () {
+(function () {
     "use strict";
 
     var $ = document.getElementById.bind(document);
 
-    var defaults = {
-        charset : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890`~!@#$%^&*()-=_+[]{}\\|;':\",.<>/?",
-        required : "",
-        index : "0",
-        length : "16"
-    };
+    var SETTINGS = ["length", "charset", "required", "index"];
+    var DEFAULTS;
 
-    var generatePassword = function () {
-        if ($("passphrase").value !== $("confirm").value) {
-            $("password").value = ("Passphrases didn't match");
-        } else {
-            var bits = sjcl.misc.pbkdf2($("passphrase").value, $("nickname").value + $("index").value);
-            var hex = sjcl.codec.hex.fromBits(bits), output = "", charset = $("charset").value, i;
+    var generatePassword = function (passphrase, domain, index, length, charset, required) {
+        var bits = sjcl.misc.pbkdf2(passphrase, domain + index);
+        var hex = sjcl.codec.hex.fromBits(bits), output = "", i;
 
-            var highBits, lowBits, index;
-            for (i = 0; i < hex.length; i += 2) {
-                highBits = parseInt(hex.charAt(i), 16) << 4;
-                lowBits = parseInt(hex.charAt(i + 1), 16);
-                index = highBits + lowBits;
-                index = Math.floor(index * charset.length / 256);
-                output += charset.charAt(index);
+        var highBits, lowBits, byte, currentCharset;
+        for (i = 0; i < hex.length; i += 2) {
+            if (i / 2 < required.length) {
+                currentCharset = required[i / 2];
+            } else {
+                currentCharset = charset;
             }
-            $("password").value = output.substr(0, parseInt($("length").value, 10));
+
+            highBits = parseInt(hex.charAt(i), 16) << 4;
+            lowBits = parseInt(hex.charAt(i + 1), 16);
+            byte = highBits + lowBits;
+            output += currentCharset.charAt(Math.floor(byte * currentCharset.length / 256));
         }
+        return output.substr(0, length);
     };
 
     var loadDomainSetting = function (domain, setting) {
         var value = localStorage.getItem("pwgen." + domain + "." + setting);
         if (value === null) {
-            value = defaults[setting];
+            if (DEFAULTS.hasOwnProperty(domain)) {
+                value = DEFAULTS[domain][setting];
+            } else {
+                value = DEFAULTS["*"][setting];
+            }
         }
         return value;
     };
 
     var loadAllDomainSettings = function () {
-        ["length", "charset", "required", "index"].forEach(function (setting) {
-            $(setting).value = loadDomainSetting($("nickname").value, setting);
+        SETTINGS.forEach(function (setting) {
+            $(setting).value = loadDomainSetting($("domain").value.trim(), setting);
         });
     };
 
     var saveDomainSetting = function (domain, setting, value) {
-        localStorage.setItem("pwgen." + domain + "." + setting, value);
+        if (value === DEFAULTS[setting]) {
+            localStorage.removeItem("pwgen." + domain + "." + setting);
+        } else {
+            localStorage.setItem("pwgen." + domain + "." + setting, value);
+        }
     };
 
     var saveDomainSettingForInput = function () {
-        saveDomainSetting($("nickname").value, this.id, this.value);
+        saveDomainSetting($("domain").value.trim(), this.id, this.value);
     };
 
-    return {
-        "generatePassword" : generatePassword,
-        "loadAllDomainSettings" : loadAllDomainSettings,
-        "saveDomainSettingForInput" : saveDomainSettingForInput
+    var onGenerateButtonClick = function () {
+        if ($("passphrase").value !== $("confirm").value) {
+            $("password").value = ("Passphrases didn't match");
+        } else {
+            $("password").value = generatePassword($("passphrase").value, $("domain").value.trim(), $("index").value.trim(), parseInt($("length").value, 10), $("charset").value.trim(), $("required").value.trim().split(/,\s*/));
+            $("password").select();
+        }
     };
+
+    // Hook up callbacks
+    window.addEventListener("load", function () {
+        $("generate").addEventListener("click", onGenerateButtonClick);
+        $("domain").addEventListener("input", loadAllDomainSettings);
+        SETTINGS.forEach(function (setting) {
+            $(setting).addEventListener("input", saveDomainSettingForInput);
+        });
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'defaults.json', true);
+        xhr.onload = function(e) {
+            if (this.status == 200) {
+                DEFAULTS = JSON.parse(this.responseText);
+                loadAllDomainSettings();
+            }
+        };
+        xhr.send();
+    });
 }());
 
-window.addEventListener("load", function () {
-    "use strict";
 
-    var $ = document.getElementById.bind(document);
-
-    $("generate").addEventListener("click", pwgen.generatePassword);
-    $("nickname").addEventListener("input", pwgen.loadAllDomainSettings);
-    ["length", "charset", "required", "index"].forEach(function (input) {
-        $(input).addEventListener("input", pwgen.saveDomainSettingForInput);
-    });
-
-    pwgen.loadAllDomainSettings();
-});
